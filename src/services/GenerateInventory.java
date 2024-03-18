9e804c0bf8901c40fed7939e14e5d7732e248e3b
Knightn4 <Justintran952@gmail.com>
2024-03-18 19:14:49 -0400

    Implemented generate invetory to create inventory arraylist

---
+++ src/services/GenerateInventory.java
@@ -0,0 +1,33 @@
+package services;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import models.Inventory;
+import models.Product;
+import models.Supplier;
+
+/* Uses the FindSupplierId class to find the supplier name 
+for each product and adds the product to the inventory list.*/  // Path: src/GenerateInventory.java
+public class GenerateInventory {
+  
+  public static ArrayList<Inventory> generateInventory(ArrayList<Product> products, ArrayList<Supplier> suppliers) {
+    ArrayList<Inventory> inventory = new ArrayList<Inventory>();
+    for (Product product : products) {
+      String supplierName = FindSupplierID.compare(suppliers, product.getProductID());
+      inventory.add(new Inventory(product.getProductID(), product.getProductName(), product.getQuantity(), product.getPrice(),product.getStatus(),supplierName));
+    }
+
+    Collections.sort(inventory, new Comparator<Inventory>() {
+      @Override
+      public int compare(Inventory i1, Inventory i2) {
+        return Integer.compare(i1.getProductID(), i2.getProductID());
+      }
+    });
+
+    return inventory;
+  }
+}
+
+
+
